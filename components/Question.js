f319eb527dc3d96cfd82dd57cb91ab986824afc1
Mingyoung Jeng <mingyoungjeng@gmail.com>
2021-04-03 23:51:26 -0500

    added quick reply to question

--- components/Question.js
+++ components/Question.js
@@ -11,19 +11,41 @@ var questions = {
       'response3': 'I know, but no one else',
       'response4': 'If I knew, we would not be here'
     }
+  },
+  'question2' : {
+    'prompt': 'How are you doing?',
+    'responses' : {
+      'response1': 'Great!',
+      'response2': 'Doing ok.',
+      'response3': 'Could be better.',
+      'response4': 'Not doing so hot.'
+    }
   }
 }
 
 class Question extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      reply: ""
+    };
+  }
+
+  revealText = () => {
+    this.setState({ reply: "Reply: yeet" });
+  };
+
   render() {
     const q= questions['question1'];
     const r = q['responses'];
     return (
       <>
         <Text>{q['prompt']}</Text>
-        <Button title={r['response1']}/>
-        <Button title={r['response2']}/>
-        <Button title={r['response3']}/>
+        <Button title={r['response1']} onPress={this.revealText}/>
+        <Button title={r['response2']} onPress={this.revealText}/>
+        <Button title={r['response3']} onPress={this.revealText}/>
+
+        <Text>{this.state.reply}</Text>
       </>
   );
   }
