bf2743ef22dceabc9277111fc82e343ee3c531d3
Mingyoung Jeng <mingyoungjeng@gmail.com>
2021-04-14 22:56:32 -0500

    im a god

--- client/components/HistoryScreen.js
+++ client/components/HistoryScreen.js
@@ -8,54 +8,59 @@ const bad = {key:'bad', color: 'red'};
 const medium = {key:'medium', color: 'yellow'}; // selectedDotColor: 'blue'}
 const good = {key:'good', color: 'green'};
 
-async function getHistory() {
-  let markedDates = {};
-  await axios.get('http://localhost:5000/api/history')
-    .then(function (res) {
-      for (var x of res.data) {
-        var date = new Date(x.date);
-        var dateString = date.getFullYear() + "-";
-
-        if ((date.getMonth() / 9) < 1) {
-          dateString += "0";
-        }
-
-        dateString += (date.getMonth() + 1) + "-";
-
-        if ((date.getDate() / 10) < 1) {
-          dateString += "0";
-        }
-
-        dateString += date.getDate();
-
-        var dots = {dots: []};
-        if (x.condition === "bad") {
-          dots.dots.push(bad);
-        }
-        if (x.condition === "medium") {
-          dots.dots.push(medium);
-        }
-        if (x.condition === "good") {
-          dots.dots.push(good);
-        }
-        
-        markedDates[dateString] = dots;
-      }
-    });
-  return markedDates;
-}
-
 class HistoryScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      markers: {}
+    };
+  }
+
+  async getHistory() {
+    let markedDates = {};
+    await axios.get('http://localhost:5000/api/history')
+      .then(function (res) {
+        for (var x of res.data) {
+          var date = new Date(x.date);
+          var dateString = date.getFullYear() + "-";
+
+          if ((date.getMonth() / 9) < 1) {
+            dateString += "0";
+          }
+
+          dateString += (date.getMonth() + 1) + "-";
+
+          if ((date.getDate() / 10) < 1) {
+            dateString += "0";
+          }
+
+          dateString += date.getDate();
+
+          var dots = {dots: []};
+          if (x.condition === "bad") {
+            dots.dots.push(bad);
+          }
+          if (x.condition === "medium") {
+            dots.dots.push(medium);
+          }
+          if (x.condition === "good") {
+            dots.dots.push(good);
+          }
+          
+          markedDates[dateString] = dots;
+        }
+      });
+    this.state.markers = markedDates;
+    this.forceUpdate();
+  }
+
   render() {
-
-    var test = getHistory();
-    console.log(test);
-
+    this.getHistory();
     return (
       <SafeAreaView style={styles.container}>
         <Calendar
-        	markedDates={test}
-        	  markingType={'multi-dot'}
+          markedDates={this.state.markers}
+            markingType={'multi-dot'}
         />
 
 
