7504b53358fb55a4f9d08bbe0352ec5a6fab62f8
johnportin <jportin13@gmail.com>
2021-04-22 00:00:49 -0500

    Add connection to mongodb

--- predictionModel/model.py
+++ predictionModel/model.py
@@ -1,8 +1,19 @@
 import pandas as pd
 from pymongo import MongoClient
 
+# GPU functionality not working right now
+import tensorflow as tf
 
-# Required dnspython
-client = pymongo.MongoClient("mongodb+srv://john:<password>@cluster0.0bzbq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-db = client.test
-print(db)
+
+# Requires dnspython
+
+# Connect to Mongo Atlas server (Don't store this info here)
+client = MongoClient("mongodb+srv://john:john1234@cluster0.0bzbq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+
+# Query database for all data and convert to list
+db = client.ciandb.data.find()
+temp = list(db)
+
+# Print all entries as a test
+for x in temp:
+    print(x)
